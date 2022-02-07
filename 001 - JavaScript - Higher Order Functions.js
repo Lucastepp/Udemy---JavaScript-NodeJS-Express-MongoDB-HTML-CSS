f6e5e5e8b5c57adf5b3fcdd1a96fa7ceb6f47193
Lucastepp <lucastep.uk@gmail.com>
2022-02-07 01:49:51 +0000

    JS Functions

---
+++ 001 - JavaScript - Higher Order Functions.js
@@ -0,0 +1,23 @@
+
+
+
+
+
+function callTwice(func) {
+    func();
+    func();
+}
+
+function callTenTimes(f) {
+    for (let i = 0; i < 10; i++) {
+        f()
+    }
+}
+
+function rollDie() {
+    const roll = Math.floor(Math.random() * 6) + 1
+    console.log(roll)
+}
+
+// Pass the function rollDie with callTwice or callTenTimes
+callTwice(rollDie)
