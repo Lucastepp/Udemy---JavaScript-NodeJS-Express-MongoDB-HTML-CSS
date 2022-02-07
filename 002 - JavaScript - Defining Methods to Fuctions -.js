d5301d6b51c672e77498f4b6df8246ea55bae93d
Lucastepp <lucastep.uk@gmail.com>
2022-02-07 04:09:54 +0000

    JS Functions

---
+++ 002 - JavaScript - Defining Methods to Fuctions -.js
@@ -0,0 +1,32 @@
+
+
+
+
+
+// Function Methods
+
+const myMath = {
+    PI: 3.14159,
+    square: function (num) {
+        return num * num;
+    },
+    cube: function (num) {
+        return num ** 3;
+    }
+}
+
+//using myMath
+myMath.square(2)
+myMath.cube(2)
+
+// shortcurt - shorthand to methods >>>>>>
+
+const myMath = {
+    PI: 3.14159,
+    square(num) {
+        return num * num;
+    },
+    cube(num) {
+        return num ** 3;
+    }
+}
