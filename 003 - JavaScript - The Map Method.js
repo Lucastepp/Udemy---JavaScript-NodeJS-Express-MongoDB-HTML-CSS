46a9ced61c9eca39ccba7eb062a374bd39dda09e
Lucastepp <lucastep.uk@gmail.com>
2022-02-08 19:09:32 +0000

    JS Functions

---
+++ 003 - JavaScript - The Map Method.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+// .map Exercise
+
+const fullNames = [{first: 'Albus', last: 'Dumbledore'}, {first: 'Harry', last: 'Potter'}, {first: 'Hermione', last: 'Granger'}, {first: 'Ron', last: 'Weasley'}, {first: 'Rubeus', last: 'Hagrid'}, {first: 'Minerva', last: 'McGonagall'}, {first: 'Severus', last: 'Snape'}];
+
+
+let firstNames = fullNames.map(function(fullName) {
+    return fullName.first;
+});
+
+console.log(firsNames);
