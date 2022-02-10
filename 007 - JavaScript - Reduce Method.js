71d0d16aa85ddf5050fc3e5819594b1c02438e29
Lucastepp <lucastep.uk@gmail.com>
2022-02-10 21:42:57 +0000

    JS Callbacks and Array Methods

---
+++ 007 - JavaScript - Reduce Method.js
@@ -0,0 +1,19 @@
+
+
+//Reduce method
+
+
+const prices = [9.99, 1.50, 19.99, 49.99]
+// for of 
+
+let total = 0;
+for (let price of prices) {
+    total += price
+}
+console.log(total)
+
+// reduce method
+
+const total = prices.reduce((total, price) => {
+    return total + price
+})
