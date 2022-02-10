e2e8fabd0a1ea18aa34cefa469d62295dc6544fe
Lucastepp <lucastep.uk@gmail.com>
2022-02-10 20:44:57 +0000

    JS Callbacks and Array Methods

--- 006 - JavaScript - Filter Exercise.js
+++ 006 - JavaScript - Filter Exercise.js
@@ -4,7 +4,7 @@
 // Write a Function called validUserNames that accepts 
 // an array of usernames. It should return a news array
 // containing only the usernames that are less than 10 chars
-
+ 
 
 function validUserNames(userNames) {
     const shortUserNames = userNames.filter(username => username.length < 10);
