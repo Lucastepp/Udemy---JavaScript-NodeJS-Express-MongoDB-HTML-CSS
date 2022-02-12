20fe966ed96469f9971da68aaa0db6dbe7bd1382
Lucastepp <lucastep.uk@gmail.com>
2022-02-12 00:10:00 +0000

    JavaScript + DOM Events

--- 009 - JavaScript - Events 001.js
+++ 009 - JavaScript - Events 001.js
@@ -1,8 +1,15 @@
 
-
+// Event Listener using function
 
 const btn = document.querySelector('#v3');
 btn.addEventListener('click', function () {
-    
+    alert('提示');
 })
 
+
+// Event Listener using Arrow function
+
+const btn4 = document.querySelector('#v4');
+btn4.addEventListener('click', () => {
+    alert('Leaving the page securely!!!');
+})
