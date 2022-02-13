e84bbe6feae07376412ea16714128f5d0567eb03
Lucastepp <lucastep.uk@gmail.com>
2022-02-13 15:22:13 +0000

    JavaScript + DOM Events

--- 016 - JavaScript - Events 008 - Ping Ping Score Keeper.js
+++ 016 - JavaScript - Events 008 - Ping Ping Score Keeper.js
@@ -7,8 +7,10 @@
 
 const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
+const resetButton = document.querySelector('#reset');
 const p1Display = document.querySelector('#p1Display');
 const p2Display = document.querySelector('#p2Display');
+const winningScoreSelect = document.querySelector('#playto');
 
 let p1Score = 0;
 let p2Score = 0;
@@ -35,5 +37,18 @@ p2Button.addEventListener('click', function () {
     }
 })
 
+winningScoreSelect.addEventListener('change', function () {
+    winningScore = parseInt(this.value);
+    reset();
+})
+
+resetButton.addEventListener('click', reset)
 
 
+function reset() {
+    isGameOver = false;
+    p1Score = 0
+    p2Score = 0
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+}
