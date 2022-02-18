4b87571bb87104bcab91d5c2e9e26d7c12d44b48
Lucastepp <lucastep.uk@gmail.com>
2022-02-18 01:38:08 +0000

    JavaScript + API

---
+++ 022 - JavaScript - APIs - Fetch API using AXIOS.js
@@ -0,0 +1,39 @@
+
+
+
+
+
+
+
+/*fetch('https://api.cryptonator.com/api/ticker/btc-usd')
+    .then(res => {
+        console.log("Response, waiting to parse...", res)
+        return res.json()
+    })
+    .then(data => {
+        console.log("Data parsed...")
+        console.log(data.ticker.price)
+    })
+    .catch(err => {
+        console.log('Oh No!!! Error!', err)
+    });
+*/
+
+/*const fetchBitcoinPrice = async () => {
+    try {
+        const res = await fetch('https://api.cryptonator.com/api/ticker/btc-usd')
+        const data = await res.json()
+        console.log(data.ticker.price)
+    } catch (err) {
+        console.log("Something Went Wrong!!!", err)
+    }
+};
+*/
+
+axios.get('https://api.cryptonator.com/api/ticker/btc-usd')
+    .then(res => {
+        console.log(res.data.ticker.price)
+    })
+    .catch (err => {
+        console.log("ERRORRR!!!", err)
+    })
