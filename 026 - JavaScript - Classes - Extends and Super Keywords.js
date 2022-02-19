f26e0716af8c1ee579ff493741e945b71a7fd9df
Lucastepp <lucastep.uk@gmail.com>
2022-02-19 00:58:46 +0000

    JavaScript + CLASS

--- 026 - JavaScript - Classes - Extends and Super Keywords.js
+++ 026 - JavaScript - Classes - Extends and Super Keywords.js
@@ -27,3 +27,15 @@ class Dog extends Pet {
         return 'WOOOF!!';
     }
 }
+
+//super keyword
+
+class Bird extends Pet {
+    constructor(name, age, lives = 3) {
+        super(name, age);
+        this.lives = lives;
+    }
+    sing() {
+        return 'Piow pioww';
+    }
+}
