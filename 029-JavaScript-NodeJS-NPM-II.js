39abd81267079a6e0e5283eef74827f2727054bf
Lucastepp <lucastep.uk@gmail.com>
2022-02-20 19:36:30 +0000

    JavaScript + NodeJS

--- 029-JavaScript-NodeJS-NPM-II.js
+++ 029-JavaScript-NodeJS-NPM-II.js
@@ -2,8 +2,9 @@
 
 
 const jokes = require('give-me-a-joke');
+const colors = require('colors');
 jokes.getRandomDadJoke (function (joke) {
-    console.log(`Second joke: ${joke}`);
+    console.log(`Second joke: ${joke.rainbow}`);
 });
 
 
@@ -12,5 +13,5 @@ const giveMeAJoke = require('give-me-a-joke');
 var fn = "Jackie";
 var ln = "Chan";
 giveMeAJoke.getCustomJoke (fn, ln, function(joke) {
-    console.log(`First joke: ${joke}`);
+    console.log(`First joke: ${joke.underline.red}`);
 });
