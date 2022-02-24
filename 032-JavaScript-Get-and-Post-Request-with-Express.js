d8d9a14e6a72bdab1d7484ad216dc79575e9e1ad
Lucastepp <lucastep.uk@gmail.com>
2022-02-24 23:12:05 +0000

    JavaScript + NodeJS / Express

--- 032-JavaScript-Get-and-Post-Request-with-Express.js
+++ 032-JavaScript-Get-and-Post-Request-with-Express.js
@@ -8,6 +8,10 @@ app.get('/tacos', (req, res) => {
     res.send("GET / tacos response")
 } )
 
+app.post('/tacos', (req, res) => {
+    res.send("POST / tacos response")
+} )
+
 app.listen(3000, () => {
     console.log('ON PORT 3000!!')
 })
