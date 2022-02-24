49db5d2cfff399b9b8edd989857a3c7dae007778
Lucastepp <lucastep.uk@gmail.com>
2022-02-24 23:49:52 +0000

    JavaScript + NodeJS / Express

--- 032-JavaScript-Get-and-Post-Request-with-Express.js
+++ 032-JavaScript-Get-and-Post-Request-with-Express.js
@@ -1,15 +1,23 @@
 
 
 
+
+
+
+
 const express = require('express');
 const app = express();
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 app.get('/tacos', (req, res) => {
     res.send("GET / tacos response")
 } )
 
 app.post('/tacos', (req, res) => {
-    res.send("POST / tacos response")
+    const { meat, qty } = req.body;
+    res.send(`POST / HELLO, HERE ARE YOUR ${qty} ${meat}`)
 } )
 
 app.listen(3000, () => {
