3bda8e5b9ee72dfbefd823cfc111126e0b6b8627
Lucastepp <lucastep.uk@gmail.com>
2022-02-26 02:10:47 +0000

    JavaScript + NodeJS / MongoDB

--- 038-MongooseBasics/038-MongooseBasics-index.js
+++ 038-MongooseBasics/038-MongooseBasics-index.js
@@ -1,3 +1,32 @@
 
-const mongoose = require('moongose');
-mongoose.connect('mongodb://localhost:27017/moviesApp', { useNewParser: true, useUnifiedTopology: true });
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/moviesApp', { useUnifiedTopology: true })
+.then(() => {
+    console.log("CONNECTION OPEN!")
+})
+.catch(err => {
+    console.log("OH NO!! ERROR!!!")
+    console.log(err)
+})
+
+const movieSchema = new mongoose.Schema({
+    title: String,
+    year: Number,
+    score: Number,
+    rating: String 
+});
+
+const Movie = mongoose.model('Movie', movieSchema);
+//const amadeus = new Movie({ title: 'Amadeuss', year: 1984, score: 9.2, rating: 'R'});
+
+Movie.insertMany([
+    {title: 'Amadeuss', year: 1984, score: 9.2, rating: 'R'},
+    {title: 'Alien', year: 1979, score: 8.1, rating: 'R'},
+    {title: 'Stand By Me', year: 1986, score: 8.6, rating: 'R'},
+    {title: 'Moonrise Kingdom', year: 2021, score: 7.3, rating: 'PG'}
+])
+    .then(data => {
+        console.log("IT WORKED!");
+        console.log(data);
+    })
+
