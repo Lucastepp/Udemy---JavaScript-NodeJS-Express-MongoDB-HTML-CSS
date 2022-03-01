7e05abc3e663ffc1fb15cf58de03473eea997de8
Lucastepp <lucastep.uk@gmail.com>
2022-03-01 20:19:31 +0000

    JavaScript + NodeJS / MongoDB

--- 039-Mongoose-shopApp/043-Mongoose-shopApp-V-Virtuals.js
+++ 039-Mongoose-shopApp/043-Mongoose-shopApp-V-Virtuals.js
@@ -12,3 +12,9 @@ const personSchema = new mongoose.Schema({
     first: String,
     last: String
 })
+
+personSchema.virtual('fullName').get(function () {
+    return `${this.first} ${this.last}`
+})
+
+const Person = mongoose.model('Person', personSchema)
