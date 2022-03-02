c67b975531d77255d181ff48dffe45ca703392b5
Lucastepp <lucastep.uk@gmail.com>
2022-03-02 19:52:33 +0000

    JavaScript + Mongoose + Express

--- 044-Mongoose-ExpressApp/044-Mongoose-ExpressApp-001.js
+++ 044-Mongoose-ExpressApp/044-Mongoose-ExpressApp-001.js
@@ -3,6 +3,15 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const { traceDeprecation } = require('process');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.get('/dog', (req, res) => {
+    res.send('WOOF!!')
+})
+
+app.listen(3000, () => {
+    console.log("APP IS LISTENING ON PORT 3000!")
+})
