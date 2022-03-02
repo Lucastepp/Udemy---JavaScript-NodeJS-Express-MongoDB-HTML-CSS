fc7ddca5d12a992b1e34d387059e04713afbede3
Lucastepp <lucastep.uk@gmail.com>
2022-03-02 20:20:56 +0000

    JavaScript + Mongoose + Express

---
+++ 044-Mongoose-ExpressApp/views/seeds.js
@@ -0,0 +1,26 @@
+
+
+const Product = require('./models/product');
+
+
+mongoose.connect('mongodb://localhost:27017/farmStand', { useUnifiedTopology: true })
+.then(() => {
+    console.log("MONGO CONNECTION OPEN!")
+})
+.catch(err => {
+    console.log("OH NO!! MONGO CONNECTION ERROR!!!")
+    console.log(err)
+});
+
+const p = new Product({
+    name: 'Ruby Grapefruit',
+    price: 1.99,
+    category: 'fruit'
+})
+
+p.save().then(p => {
+    console.log(p)
+})
+.catch(e => {
+    console.log(e)
+})
