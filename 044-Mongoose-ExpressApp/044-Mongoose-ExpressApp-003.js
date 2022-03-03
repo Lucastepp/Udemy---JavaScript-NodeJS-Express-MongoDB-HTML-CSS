16ee3f329f292e0aec021e8b91694d88504e07fc
Lucastepp <lucastep.uk@gmail.com>
2022-03-03 01:30:24 +0000

    JavaScript + Mongoose + ExpressApp

--- 044-Mongoose-ExpressApp/044-Mongoose-ExpressApp-003.js
+++ 044-Mongoose-ExpressApp/044-Mongoose-ExpressApp-003.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override')
 
 const Product = require('./models/product');
 
@@ -20,6 +21,7 @@ mongoose.connect('mongodb://localhost:27017/farmStand', { useUnifiedTopology: tr
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
 
 
 app.get('/products', async (req, res) => {
@@ -47,9 +49,16 @@ app.get('/products/:id', async (req, res) => {
 } )
 
 app.get('/products/:id/edit', async (req, res) => {
+    const { id } = req.params;
     const product = await Product.findById(id)
     res.render('products/edit', { product })
 } )
+
+app.put('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true })
+    res.redirect(`/products/${product._id}`)
+})
 
 app.listen(3000, () => {
     console.log("APP IS LISTENING ON PORT 3000!")
