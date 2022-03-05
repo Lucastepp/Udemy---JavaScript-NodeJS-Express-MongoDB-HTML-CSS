67404dc791512b23372052ca0ecbf1b4dc55f16a
Lucastepp <lucastep.uk@gmail.com>
2022-03-05 22:55:57 +0000

    JS + MongoDB + Mongoose - Relationships

--- 047-Mongo-Relationships/Models/user.js
+++ 047-Mongo-Relationships/Models/user.js
@@ -10,34 +10,56 @@ mongoose.connect('mongodb://localhost:27017/relationshipDemo', { useNewUrlParser
         console.log(err)
     })
 
-  const userSchema = new mongoose.Schema({
-      first: String,
-      last: String,
-      addresses: [
-          {
-              street: String,  
-              city: String,
-              state: String,
-              country: String
-          }
-      ]
-  })  
+const userSchema = new mongoose.Schema({
+    first: String,
+    last: String,
+    addresses: [
+        {
+            _id: { id: false },
+            street: String,
+            city: String,
+            state: String,
+            country: String
+        }
+    ]
+})
 
-  const User = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
 
-  const makeUser = async () => {
-      const u = new User({
-          first: 'Harry',
-          last: 'Potter'
-      })
-      u.addresses.push({
-          street: '123 Sesame St.',
-          city: 'New York',
-          state: 'NY',
-          country: 'USA'
-      })
-      const res = await u.save()
-      console.log(res)
-  }
+const makeUser = async () => {
+    const u = new User({
+        first: 'Harry',
+        last: 'Potter'
+    })
+    u.addresses.push({
+        street: '123 Sesame St.',
+        city: 'New York',
+        state: 'NY',
+        country: 'USA'
+    })
+    const res = await u.save()
+    console.log(res)
+}
 
-  makeUser();
+const addAddress = async (id) => {
+    const user = await User.findById(id);
+    user.addresses.push(
+        {
+            
+            street: '22 Thursan St.',
+            city: 'New York',
+            state: 'NY',
+            country: 'USA'
+
+
+        }
+    )
+    const res = await user.save()
+    console.log(res);
+}
+
+// makeUser >>
+//makeUser();
+
+//use an user id to add a new address>>
+addAddress("6223c5030a94b1f197f2e435");
