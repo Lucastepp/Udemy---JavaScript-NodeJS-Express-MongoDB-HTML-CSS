0d9512ed51c830da202823dcb97c24017152b5c3
Lucastepp <lucastep.uk@gmail.com>
2022-03-06 02:39:46 +0000

    JS + MongoDB + Express - FarmApp

--- 048-Mongoose-Express-FarmApp/app.js
+++ 048-Mongoose-Express-FarmApp/app.js
@@ -17,6 +17,7 @@ mongoose.connect('mongodb://localhost:27017/farmApp', { useUnifiedTopology: true
         console.log("OH NO!! MONGO CONNECTION ERROR!!!")
         console.log(err)
     })
+
 //-------------------------
 
 app.set('views', path.join(__dirname, 'views'));
@@ -35,12 +36,35 @@ app.get('/farms/new', (req, res) => {
     res.render('farms/new')
 })
 
+app.get('/farms/:id', async (req, res) => {
+    const farm = await Farm.findById(req.params.id).populate('products');
+    res.render('farms/show', { farm })
+})
+
 app.post('/farms', async (req, res) => {
     const farm = new Farm(req.body);
     await farm.save();
     res.redirect('/farms');
 })
 
+app.get('/farms/:id/products/new', (req, res) => {
+    const { id } = req.params;
+    res.render('products/new', { categories, id })
+})
+
+app.post('/farms/:id/products', async (req, res) => {
+    const { id } = req.params;
+    const farm = await Farm.findById(id);
+    const { name, price, category } = req.body;
+    const product = new Product({ name, price, category });
+    farm.products.push(product);
+    product.farm = farm;
+    await farm.save();
+    await product.save();
+    res.redirect(`/farms/${id}`)
+
+
+})
 // PRODUCT ROUTES----------
 
 
