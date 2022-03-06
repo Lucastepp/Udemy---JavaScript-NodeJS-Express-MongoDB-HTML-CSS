fad6d5633041caa4fe58fbd5ae36d04d11208a1e
Lucastepp <lucastep.uk@gmail.com>
2022-03-06 17:10:57 +0000

    JS + Express Routes + Cookies

---
+++ 049-Express-Router/routes/shelters.js
@@ -0,0 +1,23 @@
+
+const express = require('express');
+const { render } = require('express/lib/response');
+const router = express.Router();
+//const app = express();
+
+router.get('/shelters', (req, res) => {
+    res.send('ALL SHELTERS')
+})
+
+router.post('/shelters', (req, res) => {
+    res.send('CREATING SHELTER')
+})
+
+router.get('/shelters/:id', (req, res) => {
+    res.send('VIEWING ONE SHELTER')
+})
+
+router.get('/shelters/:id/edit', (req, res) => {
+    res.send('EDITING ONE SHELTER')
+})
+
+module.exports = router;
