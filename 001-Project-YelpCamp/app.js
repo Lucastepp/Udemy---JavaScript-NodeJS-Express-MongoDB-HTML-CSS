1cc02eb466fbbd764cd06d1780d9a8cac0063fae
Lucastepp <lucastep.uk@gmail.com>
2022-03-08 03:07:20 +0000

    JS + Express + Mongoose - YelpCamp Project

--- 001-Project-YelpCamp/app.js
+++ 001-Project-YelpCamp/app.js
@@ -2,13 +2,13 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
-
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 
-
 const campgrounds = require('./routes/campgrounds');
 const reviews = require('./routes/reviews');
+
+
 
 mongoose.connect('mongodb://localhost:27017/yelpcamp', {
     useNewUrlParser: true,
@@ -29,19 +29,12 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')))
 
 
 
 app.use('/campgrounds', campgrounds)
 app.use('/campgrounds/:id/reviews', reviews)
-
-
-
-app.get('/', (req, res) => {
-    res.render('home')
-});
-
-
 
 
 
