787bcaffbb7dd9931371fde3917d463fc86ba8da
Lucastepp <lucastep.uk@gmail.com>
2022-03-11 03:45:27 +0000

    JS + Express + Auth - YelpCamp Project

--- 001-Project-YelpCamp/routes/users.js
+++ 001-Project-YelpCamp/routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const passport = require('passport');
 const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
+const { isRef } = require('joi');
 
 
 router.get('/register', (req, res) => {
@@ -14,8 +15,12 @@ router.post('/register', catchAsync(async (req, res) => {
     const { email, username, password } = req.body;
     const user = new User({ email, username });
     const registeredUser = await User.register(user, password);
-    req.flash('success', 'Welcome to Yelp Camp!');
-    res.redirect('/campgrounds');
+    req.login(registeredUser, err => {
+        if(err) return next(err);
+        req.flash('success', 'Welcome to Yelp Camp!');
+        res.redirect('/campgrounds');
+    })
+   
     } catch (e){
         req.flash('error', e.message);
         res.redirect('register');
@@ -29,8 +34,14 @@ router.get('/login', (req, res ) => {
 
 router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
     req.flash('success', 'Welcome back!');
-    res.redirect('/campgrounds');
+    const redirectUrl = req.session.returnTo || '/campgrounds';
+    delete req.session.returnTo;
+    res.redirect(redirectUrl);
 })
 
-
+router.get('/logout', (req, res) => {
+    req.logout();
+    req.flash('success', 'Goodbye!!')
+    res.redirect('/campgrounds')
+})
 module.exports = router;
