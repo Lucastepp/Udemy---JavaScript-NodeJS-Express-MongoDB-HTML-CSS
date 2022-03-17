783b84fc845673deecb4a32435a43cc4d30cdf8d
Lucastepp <lucastep.uk@gmail.com>
2022-03-17 14:08:13 +0000

    YelpCamp

--- 053-AuthenticationDemoApp/index.js
+++ 053-AuthenticationDemoApp/index.js
@@ -20,7 +20,18 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(express.urlencoded({ extended: true }));
-app.use(session({ secret: 'notagoodsecret' }))
+app.use(session({
+    secret: 'notagoodsecret',
+    resave: true,
+    saveUninitialized: true
+}))
+
+const requireLogin = (req, res, next) => {
+    if (!req.session.user_id) {
+        res.redirect('/login')
+    }
+    next();
+}
 
 app.get('/', (req, res) => {
     res.send('THIS IS THE HOME PAGE =D')
@@ -32,30 +43,25 @@ app.get('/register', (req, res) => {
 
 app.post('/register', async (req, res) => {
     const { password, username } = req.body;
-    const hash = await bcrypt.hash(password, 12);
-    const user = new User({
-        username,
-        password: hash 
-    })
+    const user = new User({ username, password })
     await user.save();
     req.session.user_id = user._id;
     res.redirect('/')
-    
+
 })
 
 app.get('/login', (req, res) => {
     res.render('login')
 })
 
-app.post('/login', async(req, res) => {
+app.post('/login', async (req, res) => {
     const { username, password } = req.body;
-    const user = await User.findOne({ username }) //same of { username: username } !!!
-    const validPassword = await bcrypt.compare(password, user.password)
-    if (validPassword) {
-        req.session.user_id = user._id;
-        res.send('YAY, WELCOME!!!')
+    const foundUser = await User.findAndValidate(username, password);
+    if (foundUser) {
+        req.session.user_id = foundUser._id;
+        return res.redirect('/secret')
     } else {
-        res.send('SOMETHING WENT WRONG, TRY AGAIN!!!')
+        res.redirect('/login')
     }
 })
 
@@ -65,11 +71,12 @@ app.post("/logout", (req, res) => {
 
 })
 
-app.get('/secret', (req, res) => {
-    if (!req.session.user_id) {
-        res.redirect('/login')
-    }
+app.get('/secret', requireLogin, (req, res) => {
     res.render('secret')
+})
+
+app.get('/topsecret', requireLogin, (req, res) => {
+    res.render('TOP secret')
 })
 
 app.listen(3000, () => {
